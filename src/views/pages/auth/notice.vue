4786eca99555747394482b0c79446703de0fa2c0
SebastianKennedy <sebastiankennedy@foxmail.com>
2020-10-06 09:57:18 +0800

    feat: 经销商协议页面

--- src/views/pages/auth/notice.vue
+++ src/views/pages/auth/notice.vue
@@ -5,6 +5,9 @@
       <div>
         <h4 class="auth-notice-title">{{ title }}</h4>
         <div class="auth-notice-rich-text-content" v-html="richContent"></div>
+        <div class="auth-notice-agree">
+          <van-checkbox disabled shape="square" v-model="checked">我已阅读并同意该协议</van-checkbox>
+        </div>
       </div>
     </van-skeleton>
   </div>
@@ -14,15 +17,19 @@
 import Vue from "vue";
 import { goBack } from "@/router";
 import configApi from "@/api/config";
-import { NavBar, Skeleton } from "vant";
+import { NavBar, Skeleton, Checkbox, CheckboxGroup, Field } from "vant";
 
+Vue.use(Field);
 Vue.use(NavBar);
 Vue.use(Skeleton);
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
 
 export default {
   data() {
     return {
       title: "",
+      checked: true,
       richContent: "",
     };
   },
@@ -64,5 +71,9 @@ export default {
   padding-bottom: 2rem;
   max-width: 100vw;
   height: 100%;
+}
+.auth-notice-agree {
+  padding: 0 2rem;
+  padding-bottom: 2rem;
 }
 </style>
