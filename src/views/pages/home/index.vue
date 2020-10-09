2a3b2a5f37599a81cf6dfb3323782502c4fb791f
SebastianKennedy <sebastiankennedy@foxmail.com>
2020-10-09 21:54:07 +0800

    feat: 首页

--- src/views/pages/home/index.vue
+++ src/views/pages/home/index.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="home-index-page">
-    <div class="home-index-page-header">
-        <van-image width="100" height="100" src="https://img.yzcdn.cn/vant/cat.jpeg" />
-    </div>
-    <div class="home-index-page-body">
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <script>
